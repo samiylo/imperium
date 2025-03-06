416bf9a0c20c63371a1bfd2ee8aeb35c31971992
samiylo <skryshu@gmail.com>
2025-03-06 12:43:50 -0600

    Renovate: remove unused banner components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -55,20 +55,12 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 
-                <h1 className="">{`Hi! I'm Samiylo, `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ " Apps Engineer", "Web Eccommerce Architect", "UX/UI Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Machine Learning for Digital Experience</p>
               </div>}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
-                  {/* <img src={headerImg} alt="Header Img"/> */}
-                  {/* <div><BufferGeometryLines></BufferGeometryLines></div> */}
-                </div>}
-                <Spline scene="https://prod.spline.design/kcNfbALfc98G-jW2/scene.splinecode" />
-            </TrackVisibility>
+
           </Col>
         </Row>
       </Container>
